00ea48930f8ce217da028acb0023d68f33f14125
sorxcode <sorxcode@gmail.com>
2021-04-24 04:13:37 +0100

    loaded data to table

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,19 @@
 from flask import (Blueprint, abort, current_app, flash, redirect,
                    render_template, render_template_string, request, url_for, request)
-
-
+import json, os
+from pprint import pprint
 page_bp = Blueprint("page_bp", __name__, url_prefix="/pages")
 
 @page_bp.route("/<int:id>")
 def get_page(id):
-    return render_template("page.html")
+    file_path = f"static/data/page{id}.json"
+
+    if not os.path.exists(file_path):
+        return render_template("page.html")
+
+    json_data = json.load(open(file_path))
+    pprint(json_data)
+    title = list(json_data.keys())[0]
+    rows = json_data[title]
+    headings = rows[0].keys()
+    return render_template("page.html", title=title, rows=rows, headings=headings)
